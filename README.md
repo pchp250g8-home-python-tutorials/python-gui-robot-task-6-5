d06cda48d437d5d5031d1499bee9ac3a48421319
Alexander <pchp250g8@tutamail.com>
2026-02-24 21:00:28 +0300

    Created Project Task-6-5

---
+++ README.md
@@ -0,0 +1,1 @@
+RobotTask-6-5. Python Tutorials. Module Robot. StepInDev Platform. GUI Application.
---
+++ Task-6-5.py
@@ -0,0 +1,29 @@
+bDrawed = False
+
+def PaintAroundCell():
+    move_down()
+    paint()
+    move_up()
+    move_up()
+    paint()
+    move_down()
+    move_left()
+    paint()
+    move_right()
+    move_right()
+    paint()
+
+for i in range(5):
+    for j in range(5):
+        if i % 2 == 0 and is_free_right():
+            move_right()
+        elif is_free_left():
+            move_left()
+        if is_cell_painted() and not bDrawed:
+            PaintAroundCell()
+            bDrawed = True
+    if is_free_up():
+        move_up()
+
+if is_free_right():
+    move_right()        
